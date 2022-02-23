33fabaeba6d64aaaae0260e2474f9d3924f6a85d
Eric Michalsen <eric.michalsen@pantheon.io>
2022-02-23 16:10:56 -0500

    Loader comments

--- loader2.py
+++ loader2.py
@@ -20,9 +20,9 @@ sites = [["Site 1 AU", "site1-au", "au", ["Autoload", "Australia"], "<OLD_URL>",
 for site in sites:
     # switch print() for os.system()
     print("terminus site:create --org=" + org + " --region=" + site[2] + "  " + site[1] + "  \"" + site[0] + "\"  " + up_stream)
-    print("terminus pc " + site[1] + ".dev --app=mysql < " + site[1] + ".sql")
+    print("terminus pc " + site[1] + ".dev --app=mysql < " + site[1] + ".sql") # Pancakes Plugin Required
     print("terminus wp " + site[1] + ".dev search-replace " + site[6] + " '/'")
-    print("terminus rsync ./" + site[1] + "/<CONTENT>/. " + site[1] + ".dev:code/wp-content")
+    print("terminus rsync ./" + site[1] + "/<CONTENT>/. " + site[1] + ".dev:code/wp-content") # Rsync Plugin Required
     print("terminus import:files " + site[1] + ".dev " + site[4] + " /wp-content/uploads/" + site[5] + " --yes")
     for tag in site[3]:
         print("terminus tag:add " + site[1] + " " + org + " " + tag)
